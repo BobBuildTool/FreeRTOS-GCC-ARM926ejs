37ef5824cc399d222cf9a60449a350cbe43bf969
Jan Klötzke <jan@kloetzke.net>
2019-10-20 22:44:18 +0200

    receive: use echo library
    The echo implementation is moved to a dedicated library as show-case for
    cross-platform builds in the BobBuildTool.

--- Demo/receive.c
+++ Demo/receive.c
@@ -40,33 +40,10 @@
 
 #include "print.h"
 
+#include "echo.h"
 
-/* Numeric codes for special keys: */
-
-/* This code is received when BackSpace is pressed: */
-#define CODE_BS             ( 0x7F )
-/* Enter (CR): */
-#define CODE_CR             ( 0x0D )
-
-
-/* This string is displayed first when Enter is pressed: */
-#define MSG_TEXT            "You entered: \""
-/* Hardcoded strlen(MSG_TEXT): */
-#define MSG_OFFSET          ( 14 )
-/*
- * Total length of a string buffer:
- * MSG_OFFSET + RECV_BUFFER_SIZE + additional 4 characters for "\"\r\n\0"
- */
-#define RECV_TOTAL_BUFFER_LEN        ( MSG_OFFSET + RECV_BUFFER_LEN + 3 + 1 )
-
-/* Allocated "circular" buffer */
-static portCHAR buf[ RECV_BUFFER_SIZE ][ RECV_TOTAL_BUFFER_LEN ];
-
-/* Position of the currently available slot in the buffer */
-static uint16_t bufCntr = 0;
-
-/* Position of the current character within the buffer */
-static uint16_t bufPos = 0;
+/* Allocated echo buffer */
+static struct echo echo;
 
 /* UART number: */
 static uint8_t recvUartNr = ( uint8_t ) -1;
@@ -77,7 +54,6 @@ static QueueHandle_t recvQueue;
 
 /* forward declaration of an ISR handler: */
 static void recvIsrHandler(void);
-
 
 /**
  * Initializes all receive related tasks and synchronization primitives.
@@ -94,16 +70,10 @@ int16_t recvInit(uint8_t uart_nr)
     const uint8_t irq = ( uart_nr<BSP_NR_UARTS ?
                              uartIrqs[uart_nr] :
                              (uint8_t) -1 );
-    uint16_t i;
-
-    for ( i=0; i<RECV_BUFFER_SIZE; ++i )
+    if (echo_init(&echo, vPrintMsg) != 0)
     {
-        memset((void*) buf[i], '\0', RECV_TOTAL_BUFFER_LEN);
-        strcpy(buf[i], MSG_TEXT);
+        return pdFAIL;
     }
-
-    bufCntr = 0;
-    bufPos = 0;
 
     /* Check if UART number is valid */
     if ( uart_nr >= BSP_NR_UARTS )
@@ -187,138 +157,7 @@ void recvTask(void* params)
         /* The task is blocked until something appears in the queue */
         xQueueReceive(recvQueue, (void*) &ch, portMAX_DELAY);
 
-        /*
-         * Although a bit long, 'switch' offers a convenient way to
-         * insert or remove valid characters.
-         */
-        switch (ch)
-        {
-            /* "Ordinary" valid characters that will be appended to a buffer */
-
-            /* Uppercase letters 'A' .. 'Z': */
-            case 'A' :
-            case 'B' :
-            case 'C' :
-            case 'D' :
-            case 'E' :
-            case 'F' :
-            case 'G' :
-            case 'H' :
-            case 'I' :
-            case 'J' :
-            case 'K' :
-            case 'L' :
-            case 'M' :
-            case 'N' :
-            case 'O' :
-            case 'P' :
-            case 'Q' :
-            case 'R' :
-            case 'S' :
-            case 'T' :
-            case 'U' :
-            case 'V' :
-            case 'W' :
-            case 'X' :
-            case 'Y' :
-            case 'Z' :
-
-            /* Lowercase letters 'a'..'z': */
-            case 'a' :
-            case 'b' :
-            case 'c' :
-            case 'd' :
-            case 'e' :
-            case 'f' :
-            case 'g' :
-            case 'h' :
-            case 'i' :
-            case 'j' :
-            case 'k' :
-            case 'l' :
-            case 'm' :
-            case 'n' :
-            case 'o' :
-            case 'p' :
-            case 'q' :
-            case 'r' :
-            case 's' :
-            case 't' :
-            case 'u' :
-            case 'v' :
-            case 'w' :
-            case 'x' :
-            case 'y' :
-            case 'z' :
-
-            /* Decimal digits '0'..'9': */
-            case '0' :
-            case '1' :
-            case '2' :
-            case '3' :
-            case '4' :
-            case '5' :
-            case '6' :
-            case '7' :
-            case '8' :
-            case '9' :
-
-            /* Other valid characters: */
-            case ' ' :
-            case '_' :
-            case '+' :
-            case '-' :
-            case '/' :
-            case '.' :
-            case ',' :
-            {
-                if ( bufPos < RECV_BUFFER_LEN )
-                {
-                    /* If the buffer is not full yet, append the character */
-                    buf[bufCntr][MSG_OFFSET + bufPos] = ch;
-                    /* and increase the position index: */
-                    ++bufPos;
-                }
-
-                break;
-            }
-
-            /* Backspace must be handled separately: */
-            case CODE_BS :
-            {
-                /*
-                 * If the buffer is not empty, decrease the position index,
-                 * i.e. "delete" the last character
-                 */
-                if ( bufPos>0 )
-                {
-                    --bufPos;
-                }
-
-                break;
-            }
-
-            /* 'Enter' a.k.a. Carriage Return (CR): */
-            case CODE_CR :
-            {
-                /* Append characters to terminate the string:*/
-                bufPos += MSG_OFFSET;
-                buf[bufCntr][bufPos++] = '"';
-                buf[bufCntr][bufPos++] = '\r';
-                buf[bufCntr][bufPos++] = '\n';
-                buf[bufCntr][bufPos]   = '\0';
-                /* Send the entire string to the print queue */
-                vPrintMsg(buf[bufCntr]);
-                /* And switch to the next line of the "circular" buffer */
-                ++bufCntr;
-                bufCntr %= RECV_BUFFER_SIZE;
-                /* "Reset" the position index */
-                bufPos = 0;
-
-                break;
-            }
-
-        }  /* switch */
+        echo_putchar(&echo, ch);
 
     }  /* for */
 
